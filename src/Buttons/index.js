20584c7dc6fb6e0a7d47e9999ce0468c25f48baa
Kamil Soczewica <soczekv18@gmail.com>
2023-04-24 21:33:30 +0200

    added onClick for button

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,11 +1,13 @@
 import './style.css';
 
-const Buttons = ({tasksList, hideDone})=>{
-    let isEveryDone = tasksList.every(({done})=>done);
-   
-    return tasksList.length > 0 &&(
+const Buttons = ({ tasksList, hideDone, toggleHideDone }) => {
+    let isEveryDone = tasksList.every(({ done }) => done);
+
+    return tasksList.length > 0 && (
         <div className="main__buttonWrapper">
-            <button className="main__settingBtn" >{hideDone ? "Pokaż" : "Ukryji"} ukończone </button>
+            <button onClick={toggleHideDone} className="main__settingBtn" >
+                {hideDone ? "Pokaż" : "Ukryji"} ukończone
+            </button>
             <button className="main__settingBtn" disabled={isEveryDone} >Ukończ wszystkie</button>
         </div>
     )
