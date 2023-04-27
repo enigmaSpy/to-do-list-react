5c8f6376a19adf9f8c20ecb24d59db8c59383948
Kamil Soczewica <soczekv18@gmail.com>
2023-04-27 20:55:30 +0200

    added autoFocus for input

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,33 +1,39 @@
-import React, { useState } from 'react';
+import { useState,useRef } from 'react';
 import './style.css';
 const Form = ({ addNewTask }) => {
   const [newTaskContent, setNewTaskContent] = useState("");
-  const [isValid, checkIfIsValid] = useState(true);
+  const [isValid, setIsValid] = useState(true);
+  const inputFocus = useRef(null);
+  const setFocus = () => {
+    inputFocus.current.focus();
+  };
+  const requiredLength = 3;
   const onFormSubmit = (e) => {
     e.preventDefault();
-    if (!newTaskContent.trim()) {
-      checkIfIsValid(false)
+    if (!(newTaskContent.trim().length >= requiredLength)) {
+      setIsValid(false);
       return false;
     }
-    checkIfIsValid(true)
-    addNewTask(newTaskContent.trim())
-    setNewTaskContent("")
+    setIsValid(true);
+    addNewTask(newTaskContent.trim());
+    setNewTaskContent("");
   }
   return (
     <form action="" className="form" onSubmit={onFormSubmit}>
       <div className="form__inputWrapper">
         <input
+          ref={inputFocus}
           value={newTaskContent}
           type="text"
           name="newTask"
           className="form__taskInput"
           placeholder="Zadanie..."
           onChange={({ target }) => {
-            setNewTaskContent(target.value)
+            setNewTaskContent(target.value);
           }}
         />
 
-        <button type="submit" className="form__addTaskBtn">
+        <button onClick={setFocus} type="submit" className="form__addTaskBtn">
           Dodaj
         </button>
       </div>
